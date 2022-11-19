d6d00999381a99b7a9f12d6c4ef26649c7d98a4b
BrandonLogandi <contatogtgamer@gmail.com>
2022-11-19 18:22:20 -0300

    ✨feat: Combine top languages of all users

--- src/fetchers/top-languages-fetcher.js
+++ src/fetchers/top-languages-fetcher.js
@@ -51,44 +51,58 @@ const fetcher = (variables, token) => {
 };
 
 /**
- * Fetch top languages for a given username.
+ * Fetch top languages for the given usernames and consolidate them.
  *
- * @param {string} username GitHub username.
+ * @param {string} usernames GitHub usernames separated by commas.
  * @param {string[]} exclude_repo List of repositories to exclude.
  * @returns {Promise<import("./types").TopLangData>} Top languages data.
  */
-async function fetchTopLanguages(username, exclude_repo = []) {
-  if (!username) throw new MissingParamError(["username"]);
+async function fetchTopLanguages(usernames, exclude_repo = []) {
+  if (!usernames) throw new MissingParamError(["usernames"]);
 
-  const res = await retryer(fetcher, { login: username });
+  const usernamesArray = usernames.split(",");
+  let repoNodes = []; // Repo nodes of all users
 
-  if (res.data.errors) {
-    logger.error(res.data.errors);
-    throw Error(res.data.errors[0].message || "Could not fetch user");
-  }
+  for (const username of usernamesArray) {
+    const res = await retryer(fetcher, { login: username });
 
-  // Catch GraphQL errors.
-  if (res.data.errors) {
-    logger.error(res.data.errors);
-    if (res.data.errors[0].type === "NOT_FOUND") {
+    if (res.data.errors) {
+      logger.error(res.data.errors);
+      throw Error(res.data.errors[0].message || "Could not fetch user");
+    }
+
+    // Catch GraphQL errors.
+    if (res.data.errors) {
+      logger.error(res.data.errors);
+      if (res.data.errors[0].type === "NOT_FOUND") {
+        throw new CustomError(
+          res.data.errors[0].message || "Could not fetch user.",
+          CustomError.USER_NOT_FOUND,
+        );
+      }
+      if (res.data.errors[0].message) {
+        throw new CustomError(
+          wrapTextMultiline(res.data.errors[0].message, 90, 1)[0],
+          res.statusText,
+        );
+      }
       throw new CustomError(
-        res.data.errors[0].message || "Could not fetch user.",
-        CustomError.USER_NOT_FOUND,
+        "Something went while trying to retrieve the language data using the GraphQL API.",
+        CustomError.GRAPHQL_ERROR,
       );
     }
-    if (res.data.errors[0].message) {
-      throw new CustomError(
-        wrapTextMultiline(res.data.errors[0].message, 90, 1)[0],
-        res.statusText,
-      );
+
+    const userRepoNodes = res.data.data.user.repositories.nodes;
+
+    if (repoNodes.length === 0) {
+      repoNodes = userRepoNodes;
+    } else {
+      for (const node of userRepoNodes) {
+        repoNodes.push(node);
+      }
     }
-    throw new CustomError(
-      "Something went while trying to retrieve the language data using the GraphQL API.",
-      CustomError.GRAPHQL_ERROR,
-    );
   }
 
-  let repoNodes = res.data.data.user.repositories.nodes;
   let repoToHide = {};
 
   // populate repoToHide map for quick lookup
@@ -97,36 +111,38 @@ async function fetchTopLanguages(username, exclude_repo = []) {
     exclude_repo.forEach((repoName) => {
       repoToHide[repoName] = true;
     });
+
+    repoNodes = repoNodes
+      .sort((a, b) => b.size - a.size)
+      .filter((name) => !repoToHide[name.name]);
+
+    repoNodes = repoNodes
+      .filter((node) => node.languages.edges.length > 0)
+      // flatten the list of language nodes
+      .reduce((acc, curr) => curr.languages.edges.concat(acc), [])
+      .reduce((acc, prev) => {
+        // get the size of the language (bytes)
+        let langSize = prev.size;
+
+        // if we already have the language in the accumulator
+        // & the current language name is same as previous name
+        // add the size to the language size.
+        if (
+          acc[prev.node.name] &&
+          prev.node.name === acc[prev.node.name].name
+        ) {
+          langSize = prev.size + acc[prev.node.name].size;
+        }
+        return {
+          ...acc,
+          [prev.node.name]: {
+            name: prev.node.name,
+            color: prev.node.color,
+            size: langSize,
+          },
+        };
+      }, {});
   }
-
-  // filter out repositories to be hidden
-  repoNodes = repoNodes
-    .sort((a, b) => b.size - a.size)
-    .filter((name) => !repoToHide[name.name]);
-
-  repoNodes = repoNodes
-    .filter((node) => node.languages.edges.length > 0)
-    // flatten the list of language nodes
-    .reduce((acc, curr) => curr.languages.edges.concat(acc), [])
-    .reduce((acc, prev) => {
-      // get the size of the language (bytes)
-      let langSize = prev.size;
-
-      // if we already have the language in the accumulator
-      // & the current language name is same as previous name
-      // add the size to the language size.
-      if (acc[prev.node.name] && prev.node.name === acc[prev.node.name].name) {
-        langSize = prev.size + acc[prev.node.name].size;
-      }
-      return {
-        ...acc,
-        [prev.node.name]: {
-          name: prev.node.name,
-          color: prev.node.color,
-          size: langSize,
-        },
-      };
-    }, {});
 
   const topLangs = Object.keys(repoNodes)
     .sort((a, b) => repoNodes[b].size - repoNodes[a].size)
